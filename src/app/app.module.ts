11ca23a7d8292b4fcb46f1c53848c6d070c79330
Jakub Mucha <rroott33@gmail.com>
2019-11-06 06:28:51 +0100

    Angular app for transportation problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CellComponent } from './cell/cell.component';
+import { TableComponent } from './table/table.component';
+import {FormsModule} from '@angular/forms';
+import { EntityComponent } from './components/entity/entity.component';
+import { ConnectionComponent } from './components/connection/connection.component';
+import { XPipePipe } from './pipes/x-pipe.pipe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CellComponent,
+    TableComponent,
+    EntityComponent,
+    ConnectionComponent,
+    XPipePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
